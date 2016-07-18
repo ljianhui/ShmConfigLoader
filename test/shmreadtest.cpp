c7fd3381e2f84bccbeedbf0837838a20fe1d5b30
ljianhui <ljianhui2012@163.com>
2016-07-18 21:01:35 +0800

    Import test code

--- test/shmreadtest.cpp
+++ test/shmreadtest.cpp
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "shmconfigloader.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+	char configFile[256] = {'\0'};
+	char path[256] = {'\0'};
+	char *pc = strrchr(argv[0], '/');
+	memcpy(path, argv[0], pc - argv[0] + 1);
+	path[pc - argv[0] + 1] = '\0';
+	snprintf(configFile, sizeof(configFile) - 1, "%s/test.ini", path);
+
 	ShmConfigLoader configLoader(123456, MODE_READ);
-	int ret = configLoader.LoadConfig("./test.ini");
+	int ret = configLoader.LoadConfig(configFile);
 	if (ret != 0)
 	{
 		printf("Load config failed, ret = %d, errmsg[%s]\n", ret, configLoader.GetErrMsg().c_str());
